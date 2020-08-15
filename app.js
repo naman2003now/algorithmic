57a7d3356fdf68c70b1c10a47f7d6d6418ffbc51
naman2003now <69296186+naman2003now@users.noreply.github.com>
2020-08-15 12:43:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -97,3 +97,18 @@ function e(){
     document.getElementById("result").value = rep + inp
 }
 
+function copy() {
+    /* Get the text field */
+    var copyText = document.getElementById("result");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the text");
+  }
+
